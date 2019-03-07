d2296e24787f1b65938f01fc5bffa8f0e9b838da
ajay.k <ajay.k@MCITDKTP1485>
2019-03-07 14:33:00 +0530

    first commit

---
+++ src/main/java/com/example/demo/ZuulGatewayApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.config.ZuulConfigFilter;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulGatewayApplication.class, args);
+	}
+	
+	
+	@Bean
+	public ZuulConfigFilter simpleFilter() {
+		return new ZuulConfigFilter();
+	}
+
+}
---
+++ src/main/java/com/example/demo/config/ZuulConfigFilter.java
@@ -0,0 +1,31 @@
+package com.example.demo.config;
+import javax.servlet.http.HttpServletRequest;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+public class ZuulConfigFilter  extends ZuulFilter {
+	
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+	
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+	
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+	
+	@Override
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		return null;
+	}
+
+}
